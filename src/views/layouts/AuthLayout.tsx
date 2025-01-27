f904979c7520c19eec96632229371bf69ee4eb4e
Andrey Araujo <andreyaraujo.dev@gmail.com>
2025-01-27 13:16:52 -0300

    feat: create auth layout

--- src/views/layouts/AuthLayout.tsx
+++ src/views/layouts/AuthLayout.tsx
@@ -1,11 +1,33 @@
+import ilustration from '../../assets/ilustration.png';
+import { Logo } from '@/views/components/Logo';
 import { Outlet } from 'react-router-dom';
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Auth layout</h1>
+    <div className="flex w-full h-full">
+      <div className="w-full lg:w-1/2 h-full flex items-center justify-center flex-col gap-16">
+        <Logo className="text-gray-500 h-6" />
 
-      <Outlet />
+        <div className="w-full max-w-[504px] px-8">
+          <Outlet />
+        </div>
+      </div>
+      <div className="w-1/2 h-full p-8 hidden lg:flex items-center justify-center relative">
+        <img
+          src={ilustration}
+          alt="ilustration"
+          className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]"
+        />
+
+        <div className="max-w-[656px] bg-white p-10 mx-8 absolute bottom-8 rounded-b-[32px]">
+          <Logo className="text-teal-900 h-8" />
+
+          <p className="text-gray-700 font-medium text-xl mt-6">
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor,
+            totalmente de graça!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
